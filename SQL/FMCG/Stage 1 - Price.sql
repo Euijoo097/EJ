638026d7211e0cd534db7c52fdfd67999aa8a4dc
Euijoo097 <133501050+Euijoo097@users.noreply.github.com>
2026-02-14 07:29:21 +0700

    Update Stage 1 - Price.sql

--- SQL/FMCG/Stage 1 - Price.sql
+++ SQL/FMCG/Stage 1 - Price.sql
@@ -1,8 +1,10 @@
--- Quiz --
--- Top 10 MOST EXPENSIVE of order_id, product_name and price with  --
+--- Quiz 1 --
+
+/* Top 10 MOST EXPENSIVE of order_id, product_name and price with  */
 
 SELECT
     order_id, product_name, price
 FROM Purchase
 ORDER BY price DESC
+
     Limit 10
